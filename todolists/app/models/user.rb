3fbbfc4fa718acba11bec3bf009ac0e0104b61fd
Fcancel <fdcancellara@gmail.com>
2017-04-09 19:25:16 -0300

    Making thirteenth test pass.

--- todolists/app/models/user.rb
+++ todolists/app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :todo_items, through: :todo_lists, source: :todo_items, dependent: :destroy
 
   validates :username, presence:true
+
+  def get_completed_count
+    self.todo_items.where(completed: true).count
+  end
 end
