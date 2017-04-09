b44a52bab03348da98f69c8cdb4b5eb4d479737b
Fcancel <fdcancellara@gmail.com>
2017-04-09 19:17:13 -0300

    Making eleventh test pass.

--- todolists/app/models/profile.rb
+++ todolists/app/models/profile.rb
@@ -1,3 +1,21 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional:true
+
+  validates :gender, inclusion: { in: %w(male female), message: "%{value} is not a valid gender, only 'male' or 'female'" }
+
+  validate :at_least_have_name_or_last_name
+  validate :no_male_can_be_named_sue
+
+  def at_least_have_name_or_last_name
+    if first_name.nil? and last_name.nil?
+      errors.add(:profile, "Can't have both name and last name be nil.")
+    end
+  end
+
+  def no_male_can_be_named_sue
+    if first_name.eql?('Sue') and gender.eql?('male')
+      errors.add(:first_name, "%#{first_name} can't be Sue, because he is a male.")
+    end
+  end
+
 end
