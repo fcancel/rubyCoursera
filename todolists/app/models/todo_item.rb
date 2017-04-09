3c4cf2d53260dfb85670188d9f2c73a197fc4d95
Fcancel <fdcancellara@gmail.com>
2017-04-09 18:55:21 -0300

    Making tenth test pass.

--- todolists/app/models/todo_item.rb
+++ todolists/app/models/todo_item.rb
@@ -1,3 +1,5 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+
+  default_scope { order :due_date }
 end
--- todolists/app/models/todo_list.rb
+++ todolists/app/models/todo_list.rb
@@ -1,4 +1,6 @@
 class TodoList < ApplicationRecord
   belongs_to :user
   has_many :todo_items, dependent: :destroy
+
+  default_scope { order :list_due_date }
 end
