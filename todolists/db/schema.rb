f3f53c97dde5ddcd24529cff73397162d7bc73e7
Fcancel <fdcancellara@gmail.com>
2017-04-09 17:37:15 -0300

    Making third test pass.

--- todolists/db/schema.rb
+++ todolists/db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409201902) do
+ActiveRecord::Schema.define(version: 20170409203330) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "gender"
+    t.integer  "birth_year"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
