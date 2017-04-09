235f6a543391b28b9d419d2b058285fc32cd725d
Fcancel <fdcancellara@gmail.com>
2017-04-09 17:41:36 -0300

    Making fourth test pass.

--- todolists/db/schema.rb
+++ todolists/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409203330) do
+ActiveRecord::Schema.define(version: 20170409204106) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170409203330) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "list_name"
+    t.date     "list_due_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
